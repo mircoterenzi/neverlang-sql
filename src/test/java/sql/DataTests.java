7622e0541d5cd32306e54a0ee79b8c021f61e9f0
mirco <116552555+mircoterenzi@users.noreply.github.com>
2024-06-17 12:18:40 +0200

    fixed test output

--- src/test/java/sql/DataTests.java
+++ src/test/java/sql/DataTests.java
@@ -17,7 +17,10 @@ public class DataTests {
     void testInsertInto(@NeverlangUnitParam(files = "sql/add-values.sql") ASTNode node) {
         var db = node.getAttributes().get("db");
         assertInstanceOf(DatabaseMap.class, db);
-        System.out.println(((DatabaseMap) db).get("Panetteria").toString());
+        System.out.println(
+            "Database \"Panetteria\":\n" + 
+            ((DatabaseMap) db).get("Panetteria").toString()
+        );
     }
 
     @Test
